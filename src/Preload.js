b3a7148d399084d4b66696a408b6ab16f2013cd5
kklfion <fikwok@ucsc.edu>
2021-05-11 23:43:35 -0700

    Set up Preload.js and Menu.js

---
+++ src/Preload.js
@@ -0,0 +1,14 @@
+class Preload extends Phaser.Scene{
+    constructor(){
+        super("preloadScene");
+    }
+    
+    preload() {
+        
+    }
+
+    create() {
+
+        this.scene.start("menuScene");
+    }
+}
---
+++ src/scenes/Menu.js
@@ -0,0 +1,10 @@
+class Menu extends Phaser.Scene {
+    constructor() {
+        super("menuScene");
+    }
+
+    create() {
+      console.log("You are in Menu Meow");
+    }
+
+}
