927789b8f62e2f079f8d88ad1f4b813936ba8f91
kklfion <fikwok@ucsc.edu>
2021-05-12 02:35:47 -0700

    Added example for text array.

--- src/Preload.js
+++ src/Preload.js
@@ -15,6 +15,7 @@ class Preload extends Phaser.Scene{
         idea on how to load and read large amount of text:
         1. do it manually for each line - time consuming and very not efficient, but works
         2. use an array or 2D array to save all lines, will be more efficient and easier to organize
+            example for this: https://phaser.io/examples/v2/text/display-text-word-by-word
         3. somehow do it with json? I feel like there is a way to do it but idk how lmao
 
         */
