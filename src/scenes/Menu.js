ee3fb64ec419e5cbb68a1ac38efe711b886a34d5
kklfion <fikwok@ucsc.edu>
2021-05-15 03:19:57 -0700

    Added instructions in Menu.js.

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -10,8 +10,10 @@ class Menu extends Phaser.Scene {
         //create pointer
         this.mouse = this.input.activePointer;
 
-        this.add.text(game.config.width/2, game.config.height/2, 'Click to start game.', menuConfig);
-
+        this.add.text(30, game.config.height/2, 'Left click to start game.', menuConfig);
+        this.instructionText = this.add.text(30, game.config.height/1.5, '[Space] to progress,\n[left/right] to choose,\n[Q] to restart.', menuConfig);
+        this.instructionText.setFontStyle('bold');
+        this.instructionText.setFontSize(18);
     }
 
     update() {
