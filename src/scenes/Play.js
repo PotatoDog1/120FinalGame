2de1b015bf8443569fa7f3eee35818f9afd6cc4f
kklfion <fikwok@ucsc.edu>
2021-05-15 19:00:56 -0700

    Added tower art into the game.

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -38,6 +38,7 @@ class Play extends Phaser.Scene {
 
         //create frame
         this.frame = this.add.image(0, 0, 'frame').setOrigin(0, 0);
+        this.frame.depth = 1; //the default depth of every object is 0, setting the frame's depth to 1 to always keep it on top
 
         //get script
         scriptText = this.cache.json.get('json_script');
@@ -61,7 +62,7 @@ class Play extends Phaser.Scene {
                 narrativeText.setText(scriptText.crossroad1_continue[0]);
                 this.button_continue.visible = false;
                 this.button_leave.visible = false;
-                //this.placeImage = this.add.image(0, 0, 'tower').setOrigin(0, 0);
+                this.placeImage = this.add.image(0, 0, 'tower').setOrigin(0, 0);
             } else if(Phaser.Input.Keyboard.JustDown(keyLeft) && !this.continueRoute && !this.leaveRoute) {  // second part is to make sure players can't go back to the beginning of this flag
                 //leave forest route
                 this.leaveRoute = true;     //branch flag
