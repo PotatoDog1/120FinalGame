2315ca58838c62410d8b4c3d15b0588b78888426
Fion Kwok <fikwok@ucsc.edu>
2021-06-07 15:56:37 -0700

    Added Menu pic and transition.

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -13,6 +13,8 @@ class Menu extends Phaser.Scene {
         keyY = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Y);
         keyU = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.U);
 
+        this.add.image(0, 0, 'menu').setOrigin(0, 0);
+        /*
         this.add.text(30, game.config.height/2, 'Press [Space] to start game.', menuConfig);
         this.instructionText = this.add.text(30, game.config.height/1.5, 'Press [Space] to progress,\nPress [Left click] to choose.', menuConfig);
         this.instructionText.setFontStyle('bold');
@@ -20,13 +22,44 @@ class Menu extends Phaser.Scene {
         this.instructionText2 = this.add.text(30, game.config.height/1.25, 
             'Menu debug keys:\nPress [Q] to go back to Menu\nPress [E] to go to Grotto Scene\nPress [R] to go to backToGrotto Scene\nPress [T] to go to beforeBridge Scene\nPress [Y] to go to Bridge Scene\nPress [U] to go to tower Scene', menuConfig);
         this.instructionText2.setFontSize(14);
+        */
 
+        //end transition-------------------------------------------------
+        this.fog_left = this.add.sprite(-640, 0, 'fog_left').setOrigin(0, 0);
+        this.fog_left.depth = 2;
+        this.endTransition_left = this.tweens.add({
+            targets: this.fog_left,
+            //delay: 1500,
+            ease: 'Sine.easeOut',
+            duration: 1300,
+            x: 0
+        });
+        this.endTransition_left.pause();
+        
+        this.fog_right = this.add.sprite(640, 0, 'fog_right').setOrigin(0, 0);
+        this.fog_right.depth = 2;
+        this.endTransition_right = this.tweens.add({
+            targets: this.fog_right,
+            //delay: 1500,
+            ease: 'Sine.easeOut',
+            duration: 1300,
+            x: 133,
+            completeDelay: 1000,
+            onComplete: function() {
+                this.scene.start('crossroadScene');
+
+            },
+            onCompleteScope: this
+        });
+        this.endTransition_right.pause();
+
+        this.grottoTransition = false;
     }
 
     update() {
 
         if(Phaser.Input.Keyboard.JustDown(keySpace)) {
-            this.scene.start('crossroadScene');
+            this.goCrossroad();
         }
 
         if(Phaser.Input.Keyboard.JustDown(keyE)) {
@@ -131,5 +164,15 @@ class Menu extends Phaser.Scene {
             this.scene.start('towerScene');
         }
 
+    }
+
+    goCrossroad() {
+        if(!this.grottoTransition) {
+            this.fog_left.visible = true;
+            this.fog_right.visible = true;
+            this.endTransition_left.play();
+            this.endTransition_right.play();
+            this.grottoTransition = true;
+        }
     }
 }
